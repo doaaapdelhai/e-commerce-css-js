3955013c180aa13dda9e47a1b05b5db56c9e19d2
doaaapdelhai <doaaapdelhai@gmail.com>
2024-11-03 11:13:42 +0200

    finshed cart detilse page with responsive

--- js/main.js
+++ js/main.js
@@ -9,6 +9,25 @@ function close_cart(){
     cart.classList.remove("active");
 }
 
+// =====================open & close menu=================
+var menu = document.querySelector('#menu');
+
+function open_menu(){
+    menu.classList.add("active");
+}
+function close_menu(){
+    menu.classList.remove("active");
+}
+// ====================chosen img in page cart detils =====
+let bigImage = document.getElementById('bigImage');
+
+function changeItemImg(img){
+    bigImage.src=img
+
+}
+
+
+
 // =====================add items to cart=================
 var all_products_json ;
 
--- js/swiper.js
+++ js/swiper.js
@@ -25,6 +25,23 @@ var swiperSidebar = new Swiper('.slide-swp', {
         prevEl: ".swiper-button-prev",
     },
       loop:true,
+      breakpoints:{
+        1600:{
+          slidesPerView:5
+        },
+        1200:{
+          slidesPerView:5,
+          spaceBetween:30,
+        },
+        700:{
+          slidesPerView:4,
+          spaceBetween:15,
+        },
+        0:{
+          slidesPerView:2,
+          spaceBetween:10,
+        },
+      }
     });
 
 // ===========================================
@@ -40,4 +57,25 @@ var swiper = new Swiper(".product_swip", {
     prevEl: ".swiper-button-prev",
 },
   loop:true,
+  breakpoints:{
+    1500:{
+      slidesPerView:4
+    },
+    1200:{
+      slidesPerView:3,
+      spaceBetween:30,
+    },
+    900:{
+      slidesPerView:2,
+
+    },
+    700:{
+      slidesPerView:4,
+      spaceBetween:15,
+    },
+    0:{
+      slidesPerView:2,
+      spaceBetween:10,
+    },
+  }
 });
